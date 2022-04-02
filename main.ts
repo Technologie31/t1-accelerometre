37f7d78a8d3be408698b8841aeff2c1e090dc670
Technologie31 <78386526+Technologie31@users.noreply.github.com>
2022-04-02 18:53:46 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ basic.forever(function () {
     if (input.acceleration(Dimension.Y) < -100) {
         basic.showIcon(IconNames.Sad)
     } else {
-        if (input.acceleration(Dimension.Y) < 100) {
+        if (input.acceleration(Dimension.Y) > 100) {
             basic.showIcon(IconNames.Happy)
         } else {
             basic.showIcon(IconNames.Yes)
